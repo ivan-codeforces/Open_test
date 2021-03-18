f616e3edc94e7280735e9a48096c086308b54aea
Ivan Khizhkin <ivan.khizhkin@gmail.com>
2021-03-18 16:42:22 +0300

    refresh data of project

--- src/test/java/PageOpen/Main.java
+++ src/test/java/PageOpen/Main.java
@@ -1,6 +1,5 @@
 package PageOpen;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class Main extends PageBase {
@@ -9,13 +8,8 @@ public class Main extends PageBase {
         super(driver);
     }
 
-    public PageOpen.Main goTo() {
+    public void goTo() {
         driver.get(SITE_URL);
-        return this;
     }
 
-    public void chooseTask(String taskNumber) {
-        click(By.xpath("//h2[text()='Zadanie " + taskNumber + "']"));
-//        return this;
-    }
 }
